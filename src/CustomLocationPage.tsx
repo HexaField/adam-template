506ac4a0a5a5b41b2763ceb299ce3517ff21868f
HexaField <joshfield999@gmail.com>
2023-07-08 14:36:43 +1000

    update deploy action

--- src/CustomLocationPage.tsx
+++ src/CustomLocationPage.tsx
@@ -5,7 +5,7 @@ import { LoadingCircle } from '@etherealengine/client-core/src/components/Loadin
 import { EngineActions, EngineState } from '@etherealengine/engine/src/ecs/classes/EngineState'
 import { PresentationSystemGroup } from '@etherealengine/engine/src/ecs/functions/EngineFunctions'
 import { defineSystem, startSystems } from '@etherealengine/engine/src/ecs/functions/SystemFunctions'
-import { EngineRenderer } from '@etherealengine/engine/src/renderer/WebGLRendererSystem'
+import { EngineRenderer, WebGLRendererSystem } from '@etherealengine/engine/src/renderer/WebGLRendererSystem'
 import { defineState, dispatchAction, getMutableState, getState, useHookstate } from '@etherealengine/hyperflux'
 
 import 'tailwindcss/tailwind.css'
@@ -25,41 +25,6 @@ import { addObjectToGroup } from '@etherealengine/engine/src/scene/components/Gr
 import { NameComponent } from '@etherealengine/engine/src/scene/components/NameComponent'
 import { TransformComponent } from '@etherealengine/engine/src/transform/components/TransformComponent'
 import { TransformSystem } from '@etherealengine/engine/src/transform/systems/TransformSystem'
-
-const RenderSystem = defineSystem({
-  uuid: 'ee.minimalist.RenderSystem',
-  execute: () => {
-    EngineRenderer.instance.renderer.render(Engine.instance.scene, Engine.instance.camera)
-  },
-  reactor: function () {
-    useEffect(() => {
-      const onResize = () => {
-        const curPixelRatio = EngineRenderer.instance.renderer.getPixelRatio()
-        const scaledPixelRatio = window.devicePixelRatio
-
-        if (curPixelRatio !== scaledPixelRatio) EngineRenderer.instance.renderer.setPixelRatio(scaledPixelRatio)
-
-        const width = window.innerWidth
-        const height = window.innerHeight
-
-        const cam = Engine.instance.camera as PerspectiveCamera
-        cam.aspect = width / height
-        cam.updateProjectionMatrix()
-
-        EngineRenderer.instance.renderer.setSize(width, height, true)
-      }
-      onResize()
-
-      window.addEventListener('resize', onResize, false)
-
-      return () => {
-        window.removeEventListener('resize', onResize, false)
-      }
-    }, [])
-
-    return null
-  }
-})
 
 const SceneState = defineState({
   name: 'ee.minimalist.SceneState',
@@ -98,7 +63,7 @@ const UpdateSystem = defineSystem({
 
 export const startClientSystems = () => {
   startSystems([UpdateSystem], { before: TransformSystem })
-  startSystems([RenderSystem], { with: PresentationSystemGroup })
+  startSystems([WebGLRendererSystem], { with: PresentationSystemGroup })
 }
 
 export default function Template() {
