c0a062d4b8411c867d0a4ac93e660caf5d4e8de0
HexaField <joshfield999@gmail.com>
2024-06-25 10:08:27 +1000

    timer

--- src/engine.tsx
+++ src/engine.tsx
@@ -27,11 +27,13 @@ import React, { Suspense } from 'react'
 
 /** @todo due to circular dependences, engine must be imported prior to other imports */
 import { LoadingCircle } from '@etherealengine/client-core/src/components/LoadingCircle'
-import { getMutableState } from '@etherealengine/hyperflux'
+import { getMutableState, getState } from '@etherealengine/hyperflux'
 import { EngineState } from '@etherealengine/spatial/src/EngineState'
 import { createEngine } from '@etherealengine/spatial/src/initializeEngine'
+import { ECSState } from '@etherealengine/ecs'
 
 createEngine()
+getState(ECSState).timer.start()
 getMutableState(EngineState).publicPath.set(
   // @ts-ignore
   import.meta.env.BASE_URL === '/client/' ? location.origin : import.meta.env.BASE_URL!.slice(0, -1) // remove trailing '/'
